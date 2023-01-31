e7551c5f49e29e63e5538a06e4d1808f93abadfb
Valeriia Tsentylo <110411235+ValeriaTsentylo@users.noreply.github.com>
2023-01-31 17:23:03 +0200

    created second acc

--- mails/config.py
+++ mails/config.py
@@ -1,8 +1,8 @@
 ENCODING = "utf-8"
-USERNAME1 = "user@example.com"
+USERNAME1 = "user@example.com"  # UserMain in Expari
 MAIL_PASS1 = "***REMOVED***"
 
-USERNAME2 = "user2@example.com"
+USERNAME2 = "user2@example.com"         # User2 in Expari
 MAIL_PASS2 = "***REMOVED***"
 
 
