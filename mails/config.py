43dad9b700a7ee1f235c31fc52f9e6913179aa99
Valeriia Tsentylo <110411235+ValeriaTsentylo@users.noreply.github.com>
2023-03-20 15:40:26 +0200

    add csv file

--- mails/config.py
+++ mails/config.py
@@ -1,7 +1,19 @@
-ENCODING = "utf-8"
-USERNAME1 = "user@example.com"  # UserMain in Expari
-MAIL_PASS1 = "***REMOVED***"
+import csv
 
+
+ENCODING = "utf-8"
+# USERNAME1 = "user@example.com"  # UserMain in Expari
+# MAIL_PASS1 = "***REMOVED***"
+path = './login_to_mail.csv'
+with open(path, 'r') as f:
+    reader = csv.reader(f)
+    rows = list(reader)
+    last_row = rows[-1]
+    print(last_row)
+USERNAME1 = last_row[0]
+MAIL_PASS1 = last_row[1]
+print(USERNAME1)
+print(MAIL_PASS1)
 USERNAME2 = "user2@example.com"         # User2 in Expari
 MAIL_PASS2 = "***REMOVED***"
 
