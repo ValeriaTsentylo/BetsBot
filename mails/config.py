663dfccb1f75762bb6ce6802eb237c8e839a8e90
Valeriia Tsentylo <110411235+ValeriaTsentylo@users.noreply.github.com>
2023-03-24 17:42:07 +0200

    bot

--- mails/config.py
+++ mails/config.py
@@ -1,19 +1,18 @@
 import csv
 
-
 ENCODING = "utf-8"
-# USERNAME1 = "user@example.com"  # UserMain in Expari
-# MAIL_PASS1 = "***REMOVED***"
 path = './login_to_mail.csv'
 with open(path, 'r') as f:
     reader = csv.reader(f)
     rows = list(reader)
     last_row = rows[-1]
     print(last_row)
-USERNAME1 = last_row[0]
-MAIL_PASS1 = last_row[1]
-print(USERNAME1)
-print(MAIL_PASS1)
+USERNAME1 = str(last_row[0])
+MAIL_PASS1 = str(last_row[1])
+
+
+# USERNAME1 = "user@example.com"  # UserMain in Expari
+# MAIL_PASS1 = "***REMOVED***"
 USERNAME2 = "user2@example.com"         # User2 in Expari
 MAIL_PASS2 = "***REMOVED***"
 
