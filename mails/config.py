2f19cd79b8b1b1a589ae08038f4bd4103c7c7149
LightOfTheSun <vladimirov.ese@gmail.com>
2023-04-22 19:59:23 +0300

    add: global program state to keep there all necessary data (headers/logins/program settings) some typings for variables tracebacks for the internal errors in subprocesses unprocessed links will be sent to telegram instead of processing it infinite times (works only for known errors) additional command for telebot to pause program before checking new e-mails add setting for bet value maxValue and coefficient now taken from the last response (odds_info_json) add some retries on bet posting before raising an error add "/commands" command to telebot add account setup states for entering a basic user info (blog_id, bet value and waiting time before checking new e-mails)
    update:
    change absolute paths to files to relative
    replace requests for aiohttp (async analog of requests) where it's possible (didn't work with post-s for unknown reason)
    
    fix:
    link and link2 now requested only once instead of requesting for every piece of information. Html data passes between functions instead now
    user can now enter blog id, so it corresponds to user account

--- mails/config.py
+++ mails/config.py
@@ -1,18 +1,10 @@
-import csv
-
 ENCODING = "utf-8"
-path = './login_to_mail.csv'
-with open(path, 'r') as f:
-    reader = csv.reader(f)
-    rows = list(reader)
-    last_row = rows[-1]
-USERNAME1 = str(last_row[0])
-MAIL_PASS1 = str(last_row[1])
+PATH_TO_EMAIL_DATA = './login_to_mail.csv'
 
 
 # USERNAME1 = "user@example.com"  # UserMain in Expari
 # MAIL_PASS1 = "***REMOVED***"
-USERNAME2 = "user2@example.com"         # User2 in Expari
-MAIL_PASS2 = "***REMOVED***"
+# USERNAME2 = "user2@example.com"         # User2 in Expari
+# MAIL_PASS2 = "***REMOVED***"
 
 
--- telegram/config.py
+++ telegram/config.py
@@ -1,5 +1,3 @@
-import csv
-
 token = '***REMOVED***'
 path_user_id = './user_id.csv'
 path_excel = './Bets_DB_Telegram.xlsx'
@@ -7,11 +5,12 @@ path_data_for_header = './infromation_for_header.csv'
 path_mail = './login_to_mail.csv'
 
 
-with open(path_user_id, 'r') as f:  # looking for user_id for sending mails to telegram bot
-    reader = csv.reader(f)
-    rows = list(reader)
-    last_row = rows[-1]
-user_id = str(last_row[0])
+# with open(path_user_id, 'r') as f:  # looking for user_id for sending mails to telegram bot
+#     reader = csv.reader(f)
+#     rows = list(reader)
+#     last_row = rows[-1]
+
+# user_id = str(last_row[0])
 
 
 
